d1841fc12cd9c5d48ace3da723dd8789f4b5d868
Winchess1 <ikneazevmd@gmail.com>
2018-11-01 13:57:52 -0500

    Updates for my new server

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const army = [
+{id:1, name:"soldier"},
+{id:2, name:"medic"},
+{id:3, name:"engineer"},
+];
+
+
 app.get('/',(req,res)=>{
+	res.send('Hello Worlds');	
+})
 
-res.send('hello worlds');
+app.get('/army/',(req,res)=>{
+	res.send(army);	
+})
 
+app.post('/army/',(req,res)=>{
+	
+	const unit ={
+		id:army.lenght+1,
+		name:req.body.name;
+		
+	};	
+	army.push(unit);	
+	res.send(unit);
+	
 });
 
-app.listen(3000,()=>
-console.log('listenning on port 3000'));
+app.get('/army/:id/',(req,res)=>{
+	const unit = army.find(c => c.id === parseInt(req.params.id));
+	if(!unit) res.status(404).send("no such unit");
+		res.send(unit);
+})
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,(req,res)=>console.log(`Port ${port} is open`));
