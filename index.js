e9a8b59e16f36a792b5dc0e601ecc58a2c86c905
Winchess1 <ikneazevmd@gmail.com>
2018-11-02 14:51:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,65 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-const army = [
-{id:1, name:"soldier"},
-{id:2, name:"medic"},
-{id:3, name:"engineer"},
-];
-
-
-app.get('/',(req,res)=>{
-	res.send('Hello Worlds');	
-})
-
-app.get('/army/',(req,res)=>{
-	res.send(army);	
-})
-
-app.post('/army/',(req,res)=>{
-	
-	const unit ={
-		id:army.lenght+1,
-		name:req.body.name;
-		
-	};	
-	army.push(unit);	
-	res.send(unit);
-	
-});
-
-app.get('/army/:id/',(req,res)=>{
-	const unit = army.find(c => c.id === parseInt(req.params.id));
-	if(!unit) res.status(404).send("no such unit");
-		res.send(unit);
-})
-
+var http = require('http');
+var fs = require('fs');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port,(req,res)=>console.log(`Port ${port} is open`));
+http.createServer((req,res)=>{
+	
+	fs.readFile('index.html','utf8',(err,data)=>{
+	res.writeHead(200,{'content-type':'text/plain'});
+	
+	if(err) res.write('Bad connection');
+	else res.write(data);
+	res.end();
+	
+	});
+	
+}).listen(port,console.log(`Bound to ${port}`));
+
+console.log(`Port ${port} is opened`);
+
+
+const league = [
+{id:1,name:'zeus'},
+{id:2,name:'mars'},
+{id:3,name:'hulk'},
+];
+
+app.get('/',(req,res)=>{
+res.send("hellow");
+});
+
+app.get('/league/',(req,res)=>{
+	res.send(league);
+	
+});
+
+app.get('/league/:id',(req,res)=>{
+	const leagues = league.find(c => c.id === parseInt(req.params.id));
+	if(!leagues) res.status(404).send("id was not found");
+	res.send(leagues);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>
+console.log(`listenning on port ${port}`));
+
+
+
+
+1.var Http
+  var fs
+const for port
+
+2.http create aserver
+	make fs (filename,code form, func with 2 var
+
+wirte res headear ( status,attri)
+
+if err
+
+else
+
+3.listen port ( 2 attr)
+
+console.log
